091bd8bd1964584129a5ca334ddf152b25ba974d
Vasilij Orlov <vasilij_orlov@stylesoft.by>
2022-05-31 20:55:05 +0200

    initial commit

---
+++ ARFurnitureSDK.xcframework/ios-arm64/CSConnectFramework.framework/Headers/CSConnectFramework.h
@@ -0,0 +1,18 @@
+//
+//  CSConnectFramework.h
+//  CSConnectFramework
+//
+//  Created by Vasili Orlov on 27/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CSConnectFramework.
+FOUNDATION_EXPORT double CSConnectFrameworkVersionNumber;
+
+//! Project version string for CSConnectFramework.
+FOUNDATION_EXPORT const unsigned char CSConnectFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CSConnectFramework/PublicHeader.h>
+
+
